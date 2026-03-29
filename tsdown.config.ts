70b128ef2c7bd10d5ac7da3f442810ae315368cc
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-03-29 08:07:38 +0800

    chore: update config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'tsdown';
+import Starter from 'unplugin-vue/rolldown';
 
 export default defineConfig({
+  deps: {
+    neverBundle: ['vue', /^@vue\//],
+  },
   entry: './packages/src/index.ts',
-  dts: true,
+  dts: {
+    vue: true,
+  },
   minify: true,
+  platform: 'neutral',
+  // oxlint-disable-next-line new-cap
+  plugins: [Starter({ isProduction: true })],
 });
